6c0d5ed565c34238ce9c32e93785358391a9f27f
Jonathan <jonathan@dudamobile.com>
2016-06-27 09:51:28 +0300

    Break functions in calculator to reusable parts

--- src/calculator.js
+++ src/calculator.js
@@ -1,30 +1,16 @@
 /* eslint id-length: "off", no-param-reassign: "off" */
 
 /**
- * Get mortgage info. Can pass either loan amount or monthly payment, the one not
- * passed will be calculated.
- * @param  {number} loanAmount            The wanted loan amount (optional)
- * @param  {number} monthlyPayment        The wanted monthly payment (optional)
+ * Get mortgage info
+ * @param  {number} loanAmount            The wanted loan amount
  * @param  {number} numYears              Number of years for returning the mortage
- * @param  {float} yearlyInterest  The yearly interest percent
+ * @param  {float} yearlyInterest         The yearly interest percent
  * @return {object}                       An object containing data about the mortgage
  */
-export function getMortgagePartInfo({ loanAmount = 0, monthlyPayment = 0, numYears = 0, yearlyInterest = 0 }) {
-    // The yearlyInterest param will be something like 3.5 and we want to convert it to 0.035
-    const yealyInterestDecimal = yearlyInterest / 100;
-    const monthlyInterest = yealyInterestDecimal / 12;
-    const numMonths = numYears * 12;
+export function getMortgagePartInfo({ loanAmount = 0, numYears = 0, yearlyInterest = 0 }) {
+    const { numMonths } = transformParameters(numYears, yearlyInterest);
 
-    const r = 1 / (1 + monthlyInterest);
-
-    if (!monthlyPayment) {
-        monthlyPayment = loanAmount * ((1 - r) / (r - Math.pow(r, numMonths + 1)));
-    } else if (!loanAmount) {
-        loanAmount = monthlyPayment * ((r - Math.pow(r, numMonths + 1)) / (1 - r));
-    }
-
-    monthlyPayment = monthlyPayment || 0;
-    loanAmount = loanAmount || 0;
+    const monthlyPayment = getMonthlyPaymentByLoanAmount({ loanAmount, numYears, yearlyInterest });
 
     const totalPaymentToBank = numMonths * monthlyPayment;
     const costOfEachDollar = (totalPaymentToBank / loanAmount) || 0;
@@ -36,5 +22,48 @@ export function getMortgagePartInfo({ loanAmount = 0, monthlyPayment = 0, numYea
         yearlyInterest,
         totalPaymentToBank,
         costOfEachDollar
+    };
+}
+
+/**
+ * Calculate the loan amount that you can get for a given monthly payment.
+ * @param  {Number} monthlyPayment The wanted monthly payment
+ * @param  {Number} numYears       Number of years for returning the mortage
+ * @param  {Number} yearlyInterest The yearly interest percent
+ * @return The loan amount
+ */
+export function getLoanAmountByMonthlyPayment({ monthlyPayment = 0, numYears = 0, yearlyInterest = 0 }) {
+    const { numMonths, r } = transformParameters(numYears, yearlyInterest);
+
+    const loanAmount = monthlyPayment * ((r - Math.pow(r, numMonths + 1)) / (1 - r));
+    return loanAmount || 0;
+}
+
+/**
+ * Calculate the monthly payment that you will have to pay to get the given loan amount.
+ * @param  {number} loanAmount            The wanted loan amount
+ * @param  {number} numYears              Number of years for returning the mortage
+ * @param  {float} yearlyInterest         The yearly interest percent
+ * @return The monthly payment
+ */
+export function getMonthlyPaymentByLoanAmount({ loanAmount = 0, numYears = 0, yearlyInterest = 0 }) {
+    const { numMonths, r } = transformParameters(numYears, yearlyInterest);
+
+    const monthlyPayment = loanAmount * ((1 - r) / (r - Math.pow(r, numMonths + 1)));
+    return monthlyPayment || 0;
+}
+
+function transformParameters(numYears, yearlyInterest) {
+    const yealyInterestDecimal = yearlyInterest / 100;
+    const monthlyInterest = yealyInterestDecimal / 12;
+    const numMonths = numYears * 12;
+
+    const r = 1 / (1 + monthlyInterest);
+
+    return {
+        yealyInterestDecimal,
+        monthlyInterest,
+        numMonths,
+        r
     };
 }
--- test/calculatorTest.js
+++ test/calculatorTest.js
@@ -4,32 +4,8 @@ const calculator = require('../src/calculator');
 const ALLOWED_DIFFERENCE = 0.8;
 
 describe('Mashcanta calculator', function() {
-    describe('loan amount by given monthly payment', function() {
-        it('should calculate loan amount for monthly payment of 4350, interest 3.5, 15 years', function() {
-            let monthlyPayment = 4350;
-            let numYears = 15;
-            let interestRate = 3.5;
-
-            const mortgageInfo = calculator.getMortgagePartInfo({ monthlyPayment, numYears, yearlyInterest: interestRate });
-            expect(mortgageInfo.loanAmount).to.be.closeTo(608492, ALLOWED_DIFFERENCE);
-            expect(mortgageInfo.totalPaymentToBank).to.equal(783000);
-            expect(mortgageInfo.costOfEachDollar).to.be.closeTo(1.286, 0.001);
-        });
-
-        it('should calculate loan amount for monthly payment of 4000, interest 2.8, 10 years', function() {
-            let monthlyPayment = 4000;
-            let numYears = 10;
-            let interestRate = 2.8;
-
-            const mortgageInfo = calculator.getMortgagePartInfo({ monthlyPayment, numYears, yearlyInterest: interestRate });
-            expect(mortgageInfo.loanAmount).to.be.closeTo(418234, ALLOWED_DIFFERENCE);
-            expect(mortgageInfo.totalPaymentToBank).to.equal(480000);
-            expect(mortgageInfo.costOfEachDollar).to.be.closeTo(1.148, 0.001);
-        });
-    });
-
-    describe('monthly payment by given loan amount', function() {
-        it('should calculate monthly payment for loan amount of 700000, interest 3.5, 15 years', function() {
+    describe('getMortgagePartInfo', function() {
+        it('should calculate mortgage details for loan amount of 700000, interest 3.5, 15 years', function() {
             let loanAmount = 700000;
             let numYears = 15;
             let interestRate = 3.5;
@@ -40,7 +16,7 @@ describe('Mashcanta calculator', function() {
             expect(mortgageInfo.costOfEachDollar).to.be.closeTo(1.287, 0.001);
         });
 
-        it('should calculate monthly payment for loan amount of 1190000, interest 6.7, 22 years', function() {
+        it('should calculate mortgage details for loan amount of 1190000, interest 6.7, 22 years', function() {
             let loanAmount = 1190000;
             let numYears = 22;
             let interestRate = 6.7;
@@ -49,6 +25,46 @@ describe('Mashcanta calculator', function() {
             expect(mortgageInfo.monthlyPayment).to.be.closeTo(8628.21, ALLOWED_DIFFERENCE);
             expect(mortgageInfo.totalPaymentToBank).to.be.closeTo(2277847, ALLOWED_DIFFERENCE);
             expect(mortgageInfo.costOfEachDollar).to.be.closeTo(1.914, 0.001);
+        });
+    });
+
+    describe('loan amount by given monthly payment', function() {
+        it('should calculate loan amount for monthly payment of 4350, interest 3.5, 15 years', function() {
+            let monthlyPayment = 4350;
+            let numYears = 15;
+            let interestRate = 3.5;
+
+            const loanAmount = calculator.getLoanAmountByMonthlyPayment({ monthlyPayment, numYears, yearlyInterest: interestRate });
+            expect(loanAmount).to.be.closeTo(608492, ALLOWED_DIFFERENCE);
+        });
+
+        it('should calculate loan amount for monthly payment of 4000, interest 2.8, 10 years', function() {
+            let monthlyPayment = 4000;
+            let numYears = 10;
+            let interestRate = 2.8;
+
+            const loanAmount = calculator.getLoanAmountByMonthlyPayment({ monthlyPayment, numYears, yearlyInterest: interestRate });
+            expect(loanAmount).to.be.closeTo(418234, ALLOWED_DIFFERENCE);
+        });
+    });
+
+    describe('monthly payment by given loan amount', function() {
+        it('should calculate monthlyPayment for loan amount of 700000, interest 3.5, 15 years', function() {
+            let loanAmount = 700000;
+            let numYears = 15;
+            let interestRate = 3.5;
+
+            const monthlyPayment = calculator.getMonthlyPaymentByLoanAmount({ loanAmount, numYears, yearlyInterest: interestRate });
+            expect(monthlyPayment).to.be.closeTo(5004, ALLOWED_DIFFERENCE);
+        });
+
+        it('should calculate monthlyPayment for loan amount of 1190000, interest 6.7, 22 years', function() {
+            let loanAmount = 1190000;
+            let numYears = 22;
+            let interestRate = 6.7;
+
+            const monthlyPayment = calculator.getMonthlyPaymentByLoanAmount({ loanAmount, numYears, yearlyInterest: interestRate });
+            expect(monthlyPayment).to.be.closeTo(8628.21, ALLOWED_DIFFERENCE);
         });
     });
 
