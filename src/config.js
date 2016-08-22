62cf4473293ad23f1ef32af6c1fa4ef1747f8083
Jonathan <jonathan@dudamobile.com>
2016-08-22 00:40:39 -0600

    remove dummy config

--- src/config.js
+++ src/config.js
@@ -3,18 +3,15 @@
 const isProd = env === 'production';
 
 const COMMON_CONFIG = {
-    'from.common': 'moo',
-    aaaaaa: 'common'
+    
 };
 
 const DEV_CONFIG = {
-    'from.dev': 'snarf',
-    aaaaaa: 'dev'
+    
 };
 
 const PROD_CONFIG = {
-    'from.prod': 'prod',
-    aaaaaa: 'prod'
+    
 };
 
 let config;
