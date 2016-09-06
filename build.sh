b1f82dd0ce6324e1f1fa08be9fcdc5a7260e30d1
Jonathan <jonathan@dudamobile.com>
2016-09-06 10:13:30 +0300

    Create build file and automate copying to dist folder

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+NODE_ENV=production
+
+npm run clean
+
+# Copy files that need to be deployed to the public folder
+cp dev-server/* public/
+
+webpack -p --config webpack.production.config.js --progress --colors
