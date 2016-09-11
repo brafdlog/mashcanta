2fe2e03178394b36a4031e77d7cecacf78eceacb
Jonathan <jonathan@dudamobile.com>
2016-09-11 09:50:43 +0300

    Make mortgage input form a table

--- src/components/MortgageInfoInputForm.jsx
+++ src/components/MortgageInfoInputForm.jsx
@@ -1,8 +1,7 @@
 import React, { PropTypes } from 'react';
-import { List, ListItem, ListItemContent, ListItemAction, IconButton } from 'react-mdl';
+import { IconButton } from 'react-mdl';
 import cx from 'classnames';
 import './MortgageInfoInputForm.scss';
-import Flex from './Flex';
 import InfoInputCell from './InfoInputCell';
 import { formatWholeDollarAmount, formatPrecent } from '../utils';
 import { KEREN_SHAVA, SHPITZER } from '../consts';
@@ -35,46 +34,53 @@ class MortgageInfoInputForm extends React.Component {
         const { mortgageParts, className } = this.props;
         return (
             <div className={cx('MortgageInfoInputFormContainer', className)}>
-                <List>
-                    <ListItem className='headingListItem listItem' key='heading'>
-                        <ListItemContent>
-                            <Flex>
-                                <span className='columnHeading amortizationColumn'> {str('amortizationType')} </span>
-                                <span className='columnHeading amountColumn'> {str('amount')} </span>
-                                <span className='columnHeading numYearsColumn'> {str('years')} </span>
-                                <span className='columnHeading interestColumn'> {str('interest')} </span>
-                                <span className='columnHeading monthlyPaymentColumn'> {str('monthlyPayment')} </span>
-                            </Flex>
-                        </ListItemContent>
-                    </ListItem>
-                    {mortgageParts.map((part, partIndex) => {
-                        return (
-                            <ListItem className='listItem' key={part.id}>
-                                <ListItemContent>
-                                    <Flex align='center'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th className='columnHeading first'>{str('amortizationType')}</th>
+                            <th className='columnHeading'>{str('amount')}</th>
+                            <th className='columnHeading'>{str('years')}</th>
+                            <th className='columnHeading'>{str('interest')}</th>
+                            <th className='columnHeading'>{str('monthlyPayment')}</th>
+                            <th className='columnHeading'></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {mortgageParts.map((part, partIndex) => {
+                            return (
+                                <tr className='listItem' key={part.id}>
+                                    <td className='first'>
                                         <select className='column amortizationColumn' value={part.amortizationType || SHPITZER} onChange={this.buildAmortizationChangeHandler(part)}>
                                             <option value={SHPITZER}>{str('shpitzer')}</option>
                                             <option value={KEREN_SHAVA}>{str('kerenShava')}</option>
                                         </select>
+                                    </td>
+                                    <td>
                                         <InfoInputCell className='column amountColumn' content={part.loanAmount} onContentChange={this.buildChangeHandler(part, 'loanAmount')} cellFormatter={formatWholeDollarAmount} />
+                                    </td>
+                                    <td>
                                         <InfoInputCell className='column numYearsColumn' content={part.numYears} onContentChange={this.buildChangeHandler(part, 'numYears')} />
+                                    </td>
+                                    <td>
                                         <InfoInputCell className='column interestColumn' content={part.yearlyInterest} onContentChange={this.buildChangeHandler(part, 'yearlyInterest')} cellFormatter={formatPrecent} />
+                                    </td>
+                                    <td>
                                         <InfoInputCell className='column monthlyPaymentColumn' content={part.monthlyPayment} onContentChange={this.buildChangeHandler(part, 'monthlyPayment')} cellFormatter={formatWholeDollarAmount}
                                             disabled marginLeft={25}
                                         />
-                                    </Flex>
-                                </ListItemContent>
-                                <ListItemAction className='listItemAction'>
-                                    <div className='iconsContainer'>
-                                        <IconButton className='hidden-xs' name='keyboard_arrow_up' disabled={partIndex === 0} onClick={this.buildMoveHandler('up', part.id)} />
-                                        <IconButton className='hidden-xs' name='keyboard_arrow_down' disabled={partIndex === mortgageParts.length - 1} onClick={this.buildMoveHandler('down', part.id)} />
-                                        <IconButton name='delete' onClick={this.buildDeleteHandler(part.id)} />
-                                    </div>
-                                </ListItemAction>
-                            </ListItem>
-                        );
-                    })}
-                </List>
+                                    </td>
+                                    <td>
+                                        <div className='iconsContainer'>
+                                            <IconButton className='hidden-xs' name='keyboard_arrow_up' disabled={partIndex === 0} onClick={this.buildMoveHandler('up', part.id)} />
+                                            <IconButton className='hidden-xs' name='keyboard_arrow_down' disabled={partIndex === mortgageParts.length - 1} onClick={this.buildMoveHandler('down', part.id)} />
+                                            <IconButton name='delete' onClick={this.buildDeleteHandler(part.id)} />
+                                        </div>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
             </div>
         );
     }
