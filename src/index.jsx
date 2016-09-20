1d3273d516cb54150a8afb8c20ef52c839033104
Jonathan <jonathan@dudamobile.com>
2016-09-20 09:48:17 +0300

    minor

--- src/index.jsx
+++ src/index.jsx
@@ -25,7 +25,6 @@ export class App extends React.Component {
             stateStore.setMortgages(storedMortgageInfo);
             stateStore.setLoading(false);
 
-            // automatically close left panel when editos are open
             autorun('Save to storage on change', () => {
                 this.saveStateToStorage();
             });
