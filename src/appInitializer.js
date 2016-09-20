93a075eda6e51eadbdfbe7e65695c2a2154e868a
Jonathan <jonathan@dudamobile.com>
2016-09-21 00:19:01 +0300

    create an app initializer for handling things that should happen when app loads

---
+++ src/appInitializer.js
@@ -0,0 +1,21 @@
+import { autorun } from 'mobx';
+import { stateStore, USER_LOGGED_IN } from './store/StateStore';
+import { loadMortgagesFromDB, saveMortgagesToDB } from './services/storageService';
+
+autorun('Save to storage on change', () => {
+    // We don't want to save before we fetcehd the user's data from db so we don't override it by mistake.
+    if (stateStore.userStatus === USER_LOGGED_IN && stateStore.fetchedUserDataFromDb) {
+        const mortgagesToSave = stateStore.mortgages.map(mortgage => mortgage.persistableObject);
+        saveMortgagesToDB(stateStore.user.id, mortgagesToSave);
+    }
+});
+autorun('Get user data from db', () => {
+    if (stateStore.userStatus === USER_LOGGED_IN) {
+        stateStore.setLoading(true);
+        loadMortgagesFromDB(stateStore.user.id).then(storedMortgageInfo => {
+            stateStore.setMortgages(storedMortgageInfo);
+            stateStore.setFetchedUserData(true);
+            stateStore.setLoading(false);
+        });
+    }
+});
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { autorun } from 'mobx';
 import { observer } from 'mobx-react';
 import Root from './components/Root.jsx';
-import { stateStore, USER_LOGGED_IN } from './store/StateStore';
-import { getFromStorage, saveToStorage } from './services/native/storageNative';
+import { stateStore } from './store/StateStore';
+import './appInitializer';
 
 @observer
 export class App extends React.Component {
@@ -13,25 +12,6 @@ export class App extends React.Component {
         return (
             <Root stateStore={stateStore} />
 		);
-    }
-
-    componentDidMount() {
-        autorun('Save to storage on change', () => {
-            if (stateStore.userStatus === USER_LOGGED_IN && stateStore.fetchedUserDataFromDb) {
-                const mortgagesToSave = stateStore.mortgages.map(mortgage => mortgage.persistableObject);
-                saveToStorage(`user/${stateStore.user.id}/mortgageInfo`, mortgagesToSave);
-            }
-        });
-        autorun('Get user data from db', () => {
-            if (stateStore.userStatus === USER_LOGGED_IN) {
-                stateStore.setLoading(true);
-                getFromStorage(`user/${stateStore.user.id}/mortgageInfo`).then(storedMortgageInfo => {
-                    stateStore.setMortgages(storedMortgageInfo);
-                    stateStore.setFetchedUserData(true);
-                    stateStore.setLoading(false);
-                });
-            }
-        });
     }
 }
 
