1eb554af438ccf195a2a5f8858c485a878f76c01
Jonathan <jonathan@dudamobile.com>
2016-09-30 12:22:51 +0300

    Implement highlighted info line

--- src/components/InfoLine.jsx
+++ src/components/InfoLine.jsx
@@ -2,12 +2,13 @@ import React, { PropTypes } from 'react';
 import Flex from './Flex.jsx';
 import { observer } from 'mobx-react';
 import styles from './InfoLine.scss';
+import cx from 'classnames';
 
-const { string, number, oneOfType } = PropTypes;
+const { string, number, oneOfType, bool } = PropTypes;
 
 const InfoLine = observer(props => {
     return (
-        <Flex className={styles.InfoLine} align='center' justify='center'>
+        <Flex className={cx(styles.InfoLine, { [styles.highlighted]: props.highlighted })} align='center' justify='center'>
             {props.title}
             <span className={styles.value}>{props.value}</span>
         </Flex>
@@ -16,7 +17,8 @@ const InfoLine = observer(props => {
 
 InfoLine.propTypes = {
     title: string,
-    value: oneOfType([string, number])
+    value: oneOfType([string, number]),
+    highlighted: bool
 };
 
 export default InfoLine;
