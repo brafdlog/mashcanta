d0b7c2bd03d8141141d4fdf4da80a9d1c87c8755
Jonathan <jonathan@dudamobile.com>
2016-10-02 17:57:04 +0300

    better height/width handling in icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,21 +1,28 @@
 import React, { PropTypes } from 'react';
 import { observer } from 'mobx-react';
 import cx from 'classnames';
+import _ from 'lodash';
 
 const { string } = PropTypes;
 
+const addPxIfIsNumber = num => {
+    return _.isNumber(Number(num)) ? `${num}px` : num;
+};
+
 const Icon = observer(({ id, className, color = 'black', width = 35, height = 35 }) => {
-    const style = {
-        width,
-        height
+    const svgElementStyle = {
+        width: addPxIfIsNumber(width),
+        height: addPxIfIsNumber(height)
     };
+
+    const useElementStyle = {};
     if (color) {
-        style.fill = color;
+        useElementStyle.fill = color;
     }
     return (
         <span>
-            <svg className={cx(className)} >
-                <use xlinkHref={`#${id}`} style={style} />
+            <svg className={cx(className)} style={svgElementStyle}>
+                <use xlinkHref={`#${id}`} style={useElementStyle} />
             </svg>
         </span>
     );
