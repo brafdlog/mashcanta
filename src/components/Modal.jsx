0bed130c693803bfac882cb02099b2a53318c28e
Jonathan <jonathan@dudamobile.com>
2016-10-02 23:43:31 +0300

    Add to modal the option to be position fixed and z index

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import styles from './Modal.scss';
 import { observer } from 'mobx-react';
-const { string } = React.PropTypes;
+const { string, number, bool } = React.PropTypes;
 
 function Modal(props) {
     const backgroundColor = props.backgroundColor || 'rgba(0, 0, 0, 0.3)';
+    const zIndex = props.zIndex || 100;
+
+    const position = props.positionFixed ? 'fixed' : 'absolute';
     return (
-        <div className={styles.modalWrapper} style={{ backgroundColor }}>
+        <div className={styles.modalWrapper} style={{ backgroundColor, zIndex, position }}>
             {this.props.children}
         </div>
     );
 }
 
 Modal.propTypes = {
-    backgroundColor: string
+    backgroundColor: string,
+    zIndex: number,
+    positionFixed: bool
 };
 
 export default observer(Modal);
