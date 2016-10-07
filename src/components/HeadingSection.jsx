6fdb66b726b8c9ee2f6d343f98426bce7f7655b8
Jonathan <jonathan@dudamobile.com>
2016-10-07 21:28:32 +0300

    Add background image to heading and fix alignment

--- src/components/HeadingSection.jsx
+++ src/components/HeadingSection.jsx
@@ -8,12 +8,15 @@ const { func, bool } = PropTypes;
 const HeadingSection = observer(props => {
     return (
         <div className={styles.headingSection}>
-            <h1 className={styles.headingSectionTitle}> מחשבון משכנתא</h1>
-            <p className={styles.headingSectionParagraph}>מחשבון משכנתא חכם וקל לשימוש. המחשבון מראה בצורה
-            גרפית ברורה את התשלומים על המשכנתא ומאפשר
-    להשוות בקלות בין אפשרויות שונות לבניית תמהיל המשכנתא.
-            </p>
-            {props.showLoginButton ? <span className={styles.loginButton} onClick={props.handleLoginClick}> {str('login')} </span> : null}
+            <div className={styles.textContainer}>
+                <h1 className={styles.headingSectionTitle}> מחשבון משכנתא</h1>
+                <p className={styles.headingSectionParagraph}>
+                מחשבון משכנתא חכם וקל לשימוש. המחשבון מראה בצורה
+                גרפית ברורה את התשלומים על המשכנתא ומאפשר
+                להשוות בקלות בין אפשרויות שונות לבניית תמהיל המשכנתא.
+                </p>
+                {props.showLoginButton ? <div className={styles.loginButton} onClick={props.handleLoginClick}> {str('login')} </div> : null}
+            </div>
         </div>
     );
 });
